faa2fa6ea21245209d4259af186968020481986e
ccolon <c_colon10@hotmail.com>
2022-09-19 15:44:40 -0500

    fix: ajustes responsive

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-const btnInicio = document.getElementById('btn-inicio'),
-    btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
-    containerEnlacesNav = document.querySelector('.container-enlaces-nav'),
-    containerSubcategorias = document.querySelector('.container-subcategorias'),
+const btnInicio = document.getElementById('btn-inicio'), btnCerrarMenu = document.getElementById('btn-menu-cerrar'), containerEnlacesNav = document.querySelector('.container-enlaces-nav'), containerSubcategorias = document.querySelector('.container-subcategorias'),
     esDispositivoMovil = () => window.innerWidth <= 800;
 
 document.querySelectorAll('.desplegables').forEach((item) => {
     const container = document.getElementById(`${item.dataset.container}`);
-    if(container){
-        item.addEventListener('mouseover', (e) => {
-            document.querySelectorAll('.grid').forEach(grid => {
+
+    if(container === undefined || container === null) return;
+
+    item.addEventListener('click', (e) => {
+        if(container.classList.contains('activo') && !esDispositivoMovil()){
+            item.querySelector('i').classList.add('activo');
+            setTimeout(() => {
+                item.querySelector('i').classList.remove('activo');
+            }, 1000);
+        }
+        container.classList.toggle('activo');
+    });
+
+    if(esDispositivoMovil()) return;
+
+    item.addEventListener('mouseenter', (e) => {
+          document.querySelectorAll('.grid').forEach(grid => {
                 grid.classList.remove('activo');
-            });
-            container.classList.add('activo');
-        })
-    }
+          });
+          container.classList.add('activo');
+    });
+
+    container.addEventListener('mouseleave', (e) => {
+          e.target.classList.remove('activo');
+            item.querySelector('i').classList.add('activo');
+            setTimeout(() => {
+                item.querySelector('i').classList.remove('activo');
+            }, 1000);
+    });
+
 });
 
 document.querySelectorAll('.grid .btn-cerrar').forEach((button) => {
    button.addEventListener('click', () => {
         document.querySelectorAll('.grid').forEach(grid => {
           grid.classList.remove('activo');
+        });
+        document.querySelectorAll('.container-enlaces-nav').forEach(container => {
+            container.classList.remove('activo');
         });
    });
 });
